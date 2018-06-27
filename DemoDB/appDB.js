28c3c74b5b2d65c9cb2f92ca9e7ec1fce293869a
hoangfvn <damacan25@gmail.com>
2018-06-27 10:18:32 +0700

    Send Transfers to DevNet

---
+++ DemoDB/appDB.js
@@ -0,0 +1,37 @@
+const IOTA = require('iota.lib.js')
+const iota = new IOTA({ 
+    provider: "https://nodes.devnet.thetangle.org:443"
+    
+ })
+
+const remoteCurl = require('@iota/curl-remote')
+remoteCurl(iota, `https://powbox.testnet.iota.org`, 500)
+
+iota.api.getNodeInfo((error, success) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(success)
+    }
+})
+
+const trytes =
+    'HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD'
+
+const message = iota.utils.toTrytes('Hello World!')
+
+const transfers = [
+    {
+        value: 0,
+        address: trytes,
+        message: message
+    }
+]
+
+iota.api.sendTransfer(trytes, 3, 9, transfers, (error, success) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(success)
+    }
+})
